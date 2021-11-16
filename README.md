bad8a6c8fc236f0013419937bd51d12e17ebebb6
Faisal Waris <Faisal.Waris1@T-Mobile.com>
2021-11-15 21:21:38 -0500

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-TfProto
+# TfCheckpoint
+A library to extract tensor data from a Tensorflow checkpoint folder. The main use it to load pre-trained weights into model structures e.g. for PyTorch models.
+
+## Usage Example
+```F#
+
+let idxFile = @"C:\s\hack\uncased_L-2_H-128_A-2\bert_model.ckpt.index"
+
+let tensors = CheckpointReader.readCheckpoint (Path.GetDirectoryName idxFile) |> Seq.toArray
+
+let (tensorName,tensorData) = tensors.[0]
+```
+### Output:
+```
+val tensorName: string = "bert/embeddings/LayerNorm/beta"
+val tensorData: CheckpointReader.ShapedTensor =
+  { Shape = [|128L|]
+    Tensor =
+     TdFloat
+       [|0.1427177936f; 0.1417384148f; 0.1129989177f; 0.008431605063f;
+         -0.3839171827f; -0.04579306394f; -0.009391464293f; 0.2562615871f;
+         0.02031775191f; -0.1169935018f; 0.04341379181f; -0.03693608567f;
+         -0.1498966217f; -0.04671567678f; -0.05263318121f; -0.1550539136f;
+         ...
+```
+
+## Build Instructions
+
+The build relies on Tensorflow Protobuf definitions. The Tensorflow repo should be copied into the folder for the TfProto project such that the .proto file references in the .fsproj file are valid.
--- TfCheckpoint/scripts/Test.fsx
+++ TfCheckpoint/scripts/Test.fsx
@@ -1,6 +1,4 @@
-﻿
-
-#load "Packages.fsx"
+﻿#load "Packages.fsx"
 open System.IO
 open TfCheckpoint
 open System
@@ -8,25 +6,21 @@ open Google.Protobuf
 open System.Text
 open System.Runtime.InteropServices
 
-
-//let idxFile = @"C:\s\hack\uncased_L-2_H-128_A-2\bert_model.ckpt.index"
-let idxFile = @"C:\s\hack\vision_resnet-rs_resnet-rs-350-i320\model.ckpt.index"
+let idxFile = @"C:\s\hack\uncased_L-2_H-128_A-2\bert_model.ckpt.index"
+//let idxFile = @"C:\s\hack\vision_resnet-rs_resnet-rs-350-i320\model.ckpt.index"
 //let idxFile = @"C:\s\hack\uncased_L-12_H-768_A-12\bert_model.ckpt.index"
 //let idxFile = @"C:\Users\fwaris1\Downloads\4\variables\variables.index"
 //let idxFile = @"C:\s\hack\1\variables\variables.index"
 
-open CheckpointIndex
 
-let hdr,bndls = readFromFile idxFile
-let entrs = Seq.toArray bndls
-let names = entrs |> Array.map fst
+let tensors = CheckpointReader.readCheckpoint (Path.GetDirectoryName idxFile) |> Seq.toArray
 
-let ds = CheckpointReader.readCheckpoint (Path.GetDirectoryName idxFile) |> Seq.toArray
+let (tensorName,tensorData) = tensors.[0]
 
-let (n,dt) = ds.[0] 
-match dt.Tensor with
+match tensorData.Tensor with
 | CheckpointReader.TdFloat ds  -> printfn "%A" ds
 
+let header,tensorMeta = CheckpointIndex.readFromFile idxFile
 //#r "nuget: TorchSharp"
 //#r "nuget: Microsoft.ML"
 //open TorchSharp
